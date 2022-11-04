69629a2f5a4cf83202076b38e45dfe3a3fbff307
Richard Forth <richard.forth@gmail.com>
2022-11-04 18:09:24 +0000

    Create gm-foodhub-checkfridgefirst-v1.0.js

---
+++ foodhub/gm-foodhub-checkfridgefirst-v1.0.js
@@ -0,0 +1,18 @@
+// ==UserScript==
+// @name         gm-fooodhub-checkfridgefirst-banner
+// @namespace    https://foodhub.co.uk/
+// @version      1.0
+// @description  Remember to go and check the fridhe before ordering food!
+// @author       richard.forth@gmail.com
+// @match        https://foodhub.co.uk/*
+// @grant        none
+// ==/UserScript==
+
+var banner = document.createElement("div");
+banner.innerHTML = '<div style="margin: 0 auto 0 auto; ' +
+    'border-bottom: 1px solid #ff0000; margin-bottom: 5px; ' +
+    'font-size: small; background-color: lightyellow; ' +
+    'color: #000000; text-align: center;"><p style="margin: 2px 0 1px 0;"> ' +
+    'A message brought to you by Greasemonkey:<br /><strong>Remember to go and check the fridge before ordering food!</strong>' +
+    '</p></div>';
+document.body.insertBefore(banner, document.body.firstChild);
